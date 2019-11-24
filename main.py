f11203127e6304822984b1b3fca584b7503d6222
Nathan Arthur <nathan@nathanarthur.com>
2019-11-24 08:48:41 -0500

    Set lights using beeminder data

--- main.py
+++ main.py
@@ -14,52 +14,63 @@ bm = Beeminder()
 bm.set_username(config['beeminder']['user'])
 bm.set_token(config['beeminder']['token'])
 
-def get_color():
+def tick():
+    lose_date = get_lose_date()
+    pin = get_pin(lose_date)
+    interval = get_interval(lose_date)
+
+    power_light(pin, interval)
+
+def get_lose_date():
     goals = bm.get_goals()
-    lose_dates = [g['losedate'] for g in goals]
-    lose_date = min(lose_dates)
+    lose_dates = {g['slug']: g['losedate'] for g in goals}
+    key = min(lose_dates, key=lose_dates.get)
+    print(key)
+    return lose_dates[key]
+
+def get_pin(lose_date):
     today = datetime.datetime.today()
     tomorrow = today + datetime.timedelta(days=1)
     red_bound = today.replace(hour=23, minute=59, second=59).timestamp()
     yellow_bound = tomorrow.replace(hour=23, minute=59, second=59).timestamp()
 
     if lose_date <= red_bound:
-        return 'red'
+        # red
+        return 9
     elif lose_date <= yellow_bound:
-        return 'yellow'
+        # yellow
+        return 10
     else:
-        return 'green'
+        # green
+        return 11
 
-print(get_color())
+def get_interval(lose_date):
+    now = time.time()
+    remaining = lose_date - now
+    interval = lose_date / 2
+
+    return min(interval, 60 * 10)
+
+def power_light(pin, seconds):
+    if pin not in [9, 10, 11]:
+        return
+
+    for x in range(0, int(seconds / 2)):
+        GPIO.output(pin, True)
+        time.sleep(1)
+        GPIO.output(pin, False)
+        time.sleep(1)
 
 # Pin Setup:
 GPIO.setmode(GPIO.BCM)   # Broadcom pin-numbering scheme. This uses the pin numbers that match the pin numbers on the Pi Traffic light.
-
 GPIO.setup(9, GPIO.OUT)   # Red LED pin set as output
 GPIO.setup(10, GPIO.OUT)   # Yellow LED pin set as output
 GPIO.setup(11, GPIO.OUT)   # Green LED pin set as output
 
-def powerLight(pin, seconds):
-    if pin not in [9, 10, 11]:
-        return
-
-    GPIO.output(pin, True)
-    time.sleep(seconds)
-    GPIO.output(pin, False)
-
-def powerRed(seconds):
-    powerLight(9, seconds)
-
-def powerYellow(seconds):
-    powerLight(10, seconds)
-
-def powerGreen(seconds):
-    powerLight(11, seconds)
-
-powerRed(2)
-powerYellow(2)
-powerGreen(2)
-
-print('done')
-
-GPIO.cleanup()
+try:
+    while True:
+        tick()
+except KeyboardInterrupt:
+    print('Interrupted')
+finally:
+    GPIO.cleanup()
